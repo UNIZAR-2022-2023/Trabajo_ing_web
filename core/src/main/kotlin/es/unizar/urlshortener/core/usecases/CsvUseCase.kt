18bd3dc021e8b0d6adee7db10f1c0cb214c7169f
p3n4x0 <penacho.ae@gmail.com>
2022-12-21 20:20:36 +0100

    finalCsv

--- core/src/main/kotlin/es/unizar/urlshortener/core/usecases/CsvUseCase.kt
+++ core/src/main/kotlin/es/unizar/urlshortener/core/usecases/CsvUseCase.kt
@@ -1,9 +1,7 @@
 package es.unizar.urlshortener.core.usecases
 
 import es.unizar.urlshortener.core.*
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.multipart.MultipartFile
-import java.util.concurrent.BlockingQueue
 
 /**
  * Given the name of a csv file with a URL per line generates another csv file
@@ -21,19 +19,23 @@ interface CsvUseCase {
  */
 class CsvUseCaseImpl( private val createShortUrlUseCase: CreateShortUrlUseCase
 ) : CsvUseCase {
-    @Autowired
-    private val validationQueue : BlockingQueue<String>?= null
     override fun create(file: MultipartFile, data: ShortUrlProperties): String {
         var csv = String()
 
         file.inputStream.bufferedReader().forEachLine {
             csv += it
-            val shortUrl = createShortUrlUseCase.create(
-                url = it,
-                data = data
-            )
+            try{
+                val shortUrl = createShortUrlUseCase.create(
+                    url = it,
+                    data = data
+                )
+                csv += ",http://localhost:8080/${shortUrl.hash}\n"
+            }catch (e: Exception) {
+                csv += ",fallo,,Invalid URL\n"
+            }
 
-            csv += ",http://localhost:8080/${shortUrl.hash}\n"
+
+
         }
         return csv
     }
